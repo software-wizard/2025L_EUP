0caf23bb2d01d1d7c086507951aac31ec0ca5cd7
ryba24 <bartekrybarczyk2424@wp.pl>
2025-04-23 20:18:20 +0200

    Sprawdzenie czy kreatura lata

--- battle-model/src/main/java/pl/psi/SpecialField.java
+++ battle-model/src/main/java/pl/psi/SpecialField.java
@@ -1,21 +1,49 @@
 package pl.psi;
 
+
+import com.google.common.collect.Range;
 import lombok.Getter;
+import lombok.Setter;
+import pl.psi.creatures.CreatureStatistic;
+import pl.psi.creatures.CreatureStatisticIf;
 
 @Getter
-public class SpecialField
-{
+@Setter
+public class SpecialField {
     private String typeOfField;
-
-    private SpecialField(final String aTypeOfField)
-    {
+    private CreatureStatisticIf stats;
+    private boolean canFly;
+    private SpecialField(final CreatureStatisticIf aStats, final String aTypeOfField, final boolean aCanFly){
+        stats = aStats;
         typeOfField = aTypeOfField;
+        canFly = aCanFly;
     }
 
-    public static void doSomething(String typeOfField)
-    {
+    public static void doSomething(String typeOfField) {
         if (typeOfField == "fieldGivingDmg") {
             //Do something
         }
+    }
+
+    public static void canFly() {
+        String name = stats.getName();
+
+        if (name.equals("Ghost Dragon") || name.equals("Archangel") || name.equals("Efreeti") || name.equals("Gargoyle")) {
+            SpecialField.setCanFly(true);
+        }
+        else
+        {
+            SpecialField.setCanFly(false);
+        }
+    }
+
+    public static void canCreaturePassSpecialField() {
+        String name = stats.getName();
+        boolean canFly = SpecialField.getCanFly();
+        if(canFly)
+        {
+            //pass
+        }
+
     }
 }
