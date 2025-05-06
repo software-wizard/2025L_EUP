22f352a17a10bbde03e2a03d424d42aa40ce8dd8
jumidu <michal24d@op.pl>
2025-05-06 19:57:57 +0200

    usuneciezherostarychrzeczy

--- battle-model/src/main/java/pl/psi/Hero.java
+++ battle-model/src/main/java/pl/psi/Hero.java
@@ -13,53 +13,10 @@ import lombok.Getter;
  */
 public class Hero {
     private String name;
-//    @Getter
-//    private final Statistics baseStatistics;
-//    @Getter
-//    private final List<Creature> creatures;
-//
-//    private final List<Artifact> artifacts = new ArrayList<>();
 
     public Hero(String name, final List<Creature> aCreatures) {
         this.name = name;
-//        this.baseStatistics = baseStatistics;
-//        this.creatures = aCreatures;
+
     }
 
-//    public void addArtifact(Artifact artifact) {
-//        artifacts.add(artifact);
-//    }
-//
-//    public List<Artifact> getArtifacts() {
-//        return List.copyOf(artifacts);
-//    }
-//
-//    public Statistics getTotalStatistics() {
-//        Statistics total = new Statistics(
-//                baseStatistics.getAttack(),
-//                baseStatistics.getDefense(),
-//                baseStatistics.getPower(),
-//                baseStatistics.getKnowledge()
-//        );
-//        for (Artifact artifact : artifacts) {
-//            total.increase(artifact.getBonuses());
-//        }
-//        return total;
-//    }
-//
-//    public int getAttack() {
-//        return getTotalStatistics().getAttack();
-//    }
-//
-//    public int getDefense() {
-//        return getTotalStatistics().getDefense();
-//    }
-//
-//    public int getPower() {
-//        return getTotalStatistics().getPower();
-//    }
-//
-//    public int getKnowledge() {
-//        return getTotalStatistics().getKnowledge();
-//    }
 }
