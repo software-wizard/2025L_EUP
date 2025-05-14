9e1e8eb626942d9e149971bac5ff422b2f52a6c6
Erkrow <naszeryk2@gmail.com>
2025-05-14 11:38:51 +0200

    artifact enum

--- economy-model/src/main/java/pl/psi/hero/artifacts/ArtifactType.java
+++ economy-model/src/main/java/pl/psi/hero/artifacts/ArtifactType.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import pl.psi.hero.Statistics;
 
 public enum ArtifactType {
-    SWORD_OF_HELLFIRE("Sword of Hellfire", "/objects/Artifact_Sword_of_Hellfire.gif", new Statistics(6,0,0,0)),
+    SWORD_OF_HELLFIRE("Sword of Hellfire", "/objects/Artifact_Sword_of_Hellfire.gif", new Statistics(6,0,0,0),6000),
     ;
 
 
@@ -15,10 +15,13 @@ public enum ArtifactType {
     private final Statistics statistics;
     @Getter
     private final String name;
+    @Getter
+    private final int cost;
 
-    ArtifactType(String name, String imagePath, Statistics stats) {
+    ArtifactType(String name, String imagePath, Statistics stats, int cost) {
         this.name = name;
         this.imagePath = imagePath;
         this.statistics = stats;
+        this.cost = cost
     }
 }
