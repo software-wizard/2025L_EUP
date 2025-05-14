6797523a99fb12622c4df9fd94ed58d9fd5691f8
Erkrow <naszeryk2@gmail.com>
2025-05-14 08:20:38 +0200

    rewrote generators into enum
    .
    
    .

--- economy-gui/src/main/java/pl/psi/gui/EconomyBoardStart.java
+++ economy-gui/src/main/java/pl/psi/gui/EconomyBoardStart.java
@@ -15,9 +15,7 @@ import pl.psi.map.buildings.bank.Bank;
 import pl.psi.map.buildings.bank.BankStatistics;
 import pl.psi.map.resources.Gold;
 import pl.psi.map.resources.Resources;
-import pl.psi.map.resources.generators.GoldGenerator;
-import pl.psi.map.resources.generators.MercuryGenerator;
-import pl.psi.map.resources.generators.WoodGenerator;
+import pl.psi.map.resources.generators.*;
 
 import java.io.IOException;
 import java.util.Map;
@@ -50,37 +48,17 @@ public class EconomyBoardStart extends Application {
         });
     }
 
-
-    private EconomyHero hero1()
-    {
-        Statistics aStats = new Statistics(10, 10, 10, 10);
-        final EconomyHero hero1 = new EconomyHero( EconomyHero.Fraction.NECROPOLIS, new Resources(3000,50,50,50,50,50,50), aStats);
-        final EconomyNecropolisFactory factory = new EconomyNecropolisFactory();
-        hero1.addCreature( factory.create( false, 1, 1 ));
-        hero1.addCreature( factory.create( false, 1, 1 ));
-        return hero1;
-    }
-
-    private EconomyHero hero2()
-    {
-        Statistics aStats = new Statistics(10, 10, 10, 10);
-        final EconomyHero hero2 = new EconomyHero( EconomyHero.Fraction.NECROPOLIS, new Resources(4000,0,0,0,0,0,0),aStats);
-        final EconomyNecropolisFactory factory = new EconomyNecropolisFactory();
-        hero2.addCreature( factory.create( false, 2, 1 ));
-        return hero2;
-    }
-
     private Map<Point, MapObjectIf> map() {
         return Map.ofEntries(
                 Map.entry(new Point(17,1), new Castle()),
                 Map.entry(new Point(1,7), new Castle()),
-                Map.entry(new Point(3,2), new MercuryGenerator()),
-                Map.entry(new Point(5,6), new MercuryGenerator()),
-                Map.entry(new Point(8,1), new MercuryGenerator()),
-                Map.entry(new Point(10,4), new MercuryGenerator()),
-                Map.entry(new Point(13,7), new MercuryGenerator()),
-                Map.entry(new Point(15,2), new MercuryGenerator()),
-                Map.entry(new Point(6,8), new MercuryGenerator()),
+                Map.entry(new Point(3,2), new ResourceGenerator(ResourceGenType.GEM)),
+                Map.entry(new Point(5,6), new ResourceGenerator(ResourceGenType.GOLD)),
+                Map.entry(new Point(8,1), new ResourceGenerator(ResourceGenType.MERCURY)),
+                Map.entry(new Point(10,4), new ResourceGenerator(ResourceGenType.WOOD)),
+                Map.entry(new Point(13,7), new ResourceGenerator(ResourceGenType.SULFUR)),
+                Map.entry(new Point(15,2), new ResourceGenerator(ResourceGenType.CRYSTAL)),
+                Map.entry(new Point(6,8), new ResourceGenerator(ResourceGenType.ORE)),
                 Map.entry(new Point(9,3), new Gold(new Resources(1000,0,0,0,0,0,0))),
                 Map.entry(new Point(10,6), new Gold(new Resources(1000,0,0,0,0,0,0))),
                 Map.entry(new Point(2,2), new Bank(BankStatistics.CASTLE_1)),
